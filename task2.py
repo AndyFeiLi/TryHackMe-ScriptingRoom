9fe611477373d126399f6a001014b4a0c295eff9
AndyFeiLi <59521017+AndyFeiLi@users.noreply.github.com>
2020-11-28 23:43:00 -0500

    Update task2.py

--- task2.py
+++ task2.py
@@ -9,36 +9,34 @@ nextPort = 3010
 
 page = ""
 
-while nextPort != "1337":
-    
 
-    s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s= socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-    s.connect(("10.10.38.175" , 3010))
-    s.sendall(b"GET / HTTP/1.1\r\nHost: webcode.me\r\nAccept: text/html\r\nConnection: close\r\n\r\n")
+s.connect(("10.10.73.25" , 3010))
+s.sendall(b"GET / HTTP/1.1\r\nHost: webcode.me\r\nAccept: text/html\r\nConnection: close\r\n\r\n")
 
-    while True:
+while True:
 
-        data = s.recv(1024)
+    data = s.recv(1024)
 
-        if not data:
-            break
+    if not data:
+        break
 
-        page = page + data.decode()
+    page = page + data.decode()
 
 
 
     #print(page)
 
-    start = page.index("Its currenly on port") + 55
-    end = page.index("Refresh this page and it will update") - 10
+start = page.index("Its currenly on port") + 55
+end = page.index("Refresh this page and it will update") - 10
 
-    nextPort = page[start:end]
+nextPort = page[start:end]
 
-    page = ""
+page = ""
 
-    print("waiting for port 1337 to open, current port: "+ nextPort)
-    time.sleep(3)
+print("waiting for port 1337 to open, current port: "+ nextPort)
+
 
 #get next port
 
@@ -52,7 +50,7 @@ while nextPort != "9765":
 
         #print("connecting to "+nextPort)
 
-        s.connect(("10.10.38.175" , int(nextPort)))
+        s.connect(("10.10.73.25" , int(nextPort)))
         s.sendall(b"GET / HTTP/1.1\r\nHost: webcode.me\r\nAccept: text/html\r\nConnection: close\r\n\r\n")
 
 
@@ -73,16 +71,7 @@ while nextPort != "9765":
         page = ""
         time.sleep(1)
 
-        if operation == "add":
-            answer = answer + int(amount)
-        elif operation == "minus":
-            answer = answer - int(amount)
-        elif operation == "multiply":
-            answer = answer * int(amount)
-        elif operation == "divide":
-            answer = answer/int(amount)
-
-        print("current value: "+ answer)
+   
     except:
         time.sleep(0.1)
 
